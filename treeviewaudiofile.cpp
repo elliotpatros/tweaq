24245e8a2011330cac3617264a3ad736864cfc68
Elliot Patros <epatros@ucsd.edu>
2017-01-27 18:30:48 -0800

    added a hint at startup

--- treeviewaudiofile.cpp
+++ treeviewaudiofile.cpp
@@ -2,14 +2,14 @@
 
 // constructor
 TreeViewAudioFile::TreeViewAudioFile(QWidget* const parent) :
-    QTreeView(parent)
+    QTreeView(parent),
+    _hint(this)
 {
     // setup connection rules
     disconnect();
 
     // setup look and feel
     setUniformRowHeights(true);
-    setAlternatingRowColors(true);
     setIndentation(0);
     setRootIsDecorated(false);
     setItemsExpandable(false);
@@ -17,6 +17,9 @@ TreeViewAudioFile::TreeViewAudioFile(QWidget* const parent) :
     setSelectionMode(QAbstractItemView::ExtendedSelection);
     setSelectionBehavior(QAbstractItemView::SelectRows);
     sortByColumn(1);
+
+    // show hint to user
+    _hint.show(this);
 
     // setup drag and drop
     setAcceptDrops(true);
@@ -51,6 +54,31 @@ void TreeViewAudioFile::dropEvent(QDropEvent* event)
 {
     if (event->mimeData()->hasUrls())
     {
+        _hint.hide();
         emit droppedUrls(event->mimeData()->urls());
     }
+}
+
+
+StartupHint::StartupHint(QWidget* const parent) :
+    _layout(new QVBoxLayout(parent)),
+    _label(new QLabel("drop files and folders", parent))
+{
+    _layout->addWidget(_label, 0, Qt::AlignCenter);
+
+    _label->setAlignment(Qt::AlignCenter);
+    _label->setStyleSheet(QStringLiteral("font:normal; color:rgba(0, 0, 0, 15%); font-size:32px;"));
+    _label->setEnabled(false);
+    _label->setAttribute(Qt::WA_TransparentForMouseEvents);
+}
+
+void StartupHint::show(QWidget* widget)
+{
+    widget->setLayout(_layout);
+}
+
+void StartupHint::hide()
+{
+    delete _label;
+    _label = nullptr;
 }
--- treeviewaudiofile.h
+++ treeviewaudiofile.h
@@ -13,6 +13,25 @@
 #include <QDropEvent>
 #include <QMimeData>
 
+// widgets
+#include <QLabel>
+#include <QVBoxLayout>
+
+class StartupHint
+{
+public:
+    explicit StartupHint(QWidget* const parent = nullptr);
+
+    // sets
+    void show(QWidget* widget);
+    void hide();
+
+
+private:
+    QVBoxLayout* _layout;
+    QLabel* _label;
+};
+
 class TreeViewAudioFile : public QTreeView
 {
     Q_OBJECT
@@ -33,6 +52,10 @@ protected:
     void dragEnterEvent(QDragEnterEvent* event);
     void dragMoveEvent(QDragMoveEvent* event);
     void dropEvent(QDropEvent* event);
+
+
+private:
+    StartupHint _hint;
 };
 
 #endif // TREEVIEWAUDIOFILE_H
